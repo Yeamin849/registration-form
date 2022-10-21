1a63bd7c767a48e685662a67f31d5ba64def2a82
Yeamin Hossen <62115757+Yeamin849@users.noreply.github.com>
2022-10-22 01:36:28 +0600

    reg.form

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<?php
+/*
+$uname = $_POST['Username'];
+$e = $_POST['email'];
+$pass = $_POST['Passward'];
+$m = $_POST['Mobile'];
+
+$fh = fopen("userinfo.txt","at");
+$line = $uname."|".$e."|".$pass."|".$m."|\n";
+
+fwrite($fh,$line);
+fclose($fh);
+echo "thanks for registaration";
+*/
+?>
